807a2ab03120b48d1ab8bd4bb8005d80ca8622ab
JoeltonLP <joelton.mpto.dev@gmail.com>
2021-04-24 15:53:48 -0300

    Foi implementado o verbo GET de state .
    closes #3

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import state_index
 
+from django.urls import path
+from .views import state_index, state_by_pk
+
 urlpatterns = [
-    path('states', state_index)
+    path('states', state_index),
+    path('states/<int:pk>', state_by_pk)
 ]
