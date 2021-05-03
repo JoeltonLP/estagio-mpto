6a41d7dfc4b2a1cab21efe84d8c75a4bae4c9ba5
JoeltonLP <joelton.mpto.dev@gmail.com>
2021-05-03 12:38:56 -0300

    Foi implementado o endpoint para tratar person
    closes #21

--- tracker/urls.py
+++ tracker/urls.py
@@ -10,7 +10,9 @@ from .views import (
     natural_person_index,
     natural_person_by_id,
     legal_person_index,
-    legal_person_by_id
+    legal_person_by_id,
+    person_index,
+    person_by_id
 )
 
 urlpatterns = [
@@ -19,6 +21,8 @@ urlpatterns = [
     path('cities', city_index),
     path('cities/<int:id>', city_by_id),
     path('natural-persons', natural_person_index),
+    path('persons', person_index),
+    path('persons/<int:id>', person_by_id),
     path('natural-persons/<int:id>', natural_person_by_id),
     path('legal-persons', legal_person_index),
     path('legal-persons/<int:id>', legal_person_by_id)
